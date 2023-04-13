2048655eef71c7c9d4f7eb522f7d5678d09f1bce
Dufyz <guilhermeiagothomaz@hotmail.com>
2023-04-13 16:54:15 -0300

    observer e façade

---
+++ designPatterns/behavioural/observer.py
@@ -0,0 +1,5 @@
+# define uma dependência um para muitos entre objetos, de modo que, quando um objeto muda de estado,
+# todos os seus dependentes são automaticamente notificados e atualizados.
+
+from __future__ import annotations
+
